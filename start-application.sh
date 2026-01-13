a6ca8ea053db0adaa809ab1bd44cf758b47964ab
cgsphere-global <cgsphere.dev@gmail.com>
2026-01-14 03:42:10 +0530

    ..

--- start-application.sh
+++ start-application.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 echo "Starting CG Sphere Model..."
-cd /workspace
-exec uvicorn application:app --host 0.0.0.0 --port 8000
+exec uvicorn application:app --host 0.0.0.0 --port 8000 --log-level debug
